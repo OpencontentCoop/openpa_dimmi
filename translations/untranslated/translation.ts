f5e15cbbd21fea7989e346a47a02b167a272f404
Luca Realdi <lr@opencontent.it>
2016-12-02 00:28:25 +0100

    init translations

---
+++ translations/untranslated/translation.ts
@@ -0,0 +1,376 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>design/admin/content/edit</name>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="4"/>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="24"/>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="113"/>
+        <source>Send for publishing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="24"/>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="113"/>
+        <source>Publish the contents of the draft that is being edited. The draft will become the published version of the object.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="25"/>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="114"/>
+        <source>Store draft</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="25"/>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="114"/>
+        <source>Store the contents of the draft that is being edited and continue editing. Use this button to periodically save your work while editing.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="26"/>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="115"/>
+        <source>Store draft and exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="26"/>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="115"/>
+        <source>Store the draft that is being edited and exit from edit mode. Use when you need to exit your work and return later to continue.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="27"/>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="116"/>
+        <source>Discard draft</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="27"/>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="116"/>
+        <source>Are you sure you want to discard the draft?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="27"/>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="116"/>
+        <source>Discard the draft that is being edited. This will also remove the translations that belong to the draft (if any).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="30"/>
+        <source>Toggle fullscreen editing!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="48"/>
+        <source>Edit &lt;%object_name&gt; (%class_name)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="77"/>
+        <source>Translating content from %from_lang to %to_lang</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/admin/override/templates/reply_edit.tpl" line="111"/>
+        <source>Back to edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/override/templates/reply_edit.tpl" line="39"/>
+        <source>Required data is either missing or is invalid</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/override/templates/reply_edit.tpl" line="48"/>
+        <source>The following locations are invalid</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/override/templates/reply_edit.tpl" line="57"/>
+        <source>The following data is invalid according to the custom validation rules</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>design/admin/content/edit_attribute</name>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/override/templates/reply_edit.tpl" line="126"/>
+        <source>not translatable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/override/templates/reply_edit.tpl" line="145"/>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/override/templates/reply_edit.tpl" line="170"/>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/override/templates/reply_edit.tpl" line="183"/>
+        <source>required</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/override/templates/reply_edit.tpl" line="146"/>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/override/templates/reply_edit.tpl" line="171"/>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/override/templates/reply_edit.tpl" line="184"/>
+        <source>information collector</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>design/ocbootstrap/full/forum_topic</name>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/dimmi/forum/reply.tpl" line="24"/>
+        <source>Moderated by</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/dimmi/forum/topic.tpl" line="82"/>
+        <source>Inserisci commento</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/dimmi/forum/topic.tpl" line="89"/>
+        <source>Keep me updated</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/content/edit</name>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/override/templates/reply_edit.tpl" line="205"/>
+        <source>Discard</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/notification</name>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/standard/templates/openpa/mail_ezsubtreenotification/_default.tpl" line="12"/>
+        <location filename="../../../../../../../git/openpa_dimmi/design/standard/templates/openpa/mail_ezsubtreenotification/dimmi_forum_reply.tpl" line="25"/>
+        <source>This email is to inform you that an updated item has been published at %sitename.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/standard/templates/openpa/mail_ezsubtreenotification/_default.tpl" line="13"/>
+        <location filename="../../../../../../../git/openpa_dimmi/design/standard/templates/openpa/mail_ezsubtreenotification/_default.tpl" line="26"/>
+        <source>The item can be viewed by using the URL below.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/standard/templates/openpa/mail_ezsubtreenotification/_default.tpl" line="25"/>
+        <location filename="../../../../../../../git/openpa_dimmi/design/standard/templates/openpa/mail_ezsubtreenotification/dimmi_forum_reply.tpl" line="43"/>
+        <source>This email is to inform you that a new item has been published at %sitename.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dimmi</name>
+    <message>
+        <location filename="control_dimmi.php" line="403"/>
+        <source>Dimmi - progetto di riuso del Consorzio dei Comuni Trentini - realizzato da Opencontent con ComunWeb</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dimmi/config</name>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/dimmi/config.tpl" line="17"/>
+        <source>Moderazione attivata</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/dimmi/config.tpl" line="25"/>
+        <source>Modifica impostazioni generali</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/dimmi/config.tpl" line="26"/>
+        <source>Modifica informazioni Dimmi</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/dimmi/config.tpl" line="35"/>
+        <source>Utenti</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/dimmi/config.tpl" line="69"/>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/dimmi/config.tpl" line="113"/>
+        <source>Cerca</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/dimmi/config.tpl" line="94"/>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/dimmi/config.tpl" line="136"/>
+        <source>Esporta in CSV</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/dimmi/config.tpl" line="137"/>
+        <source>Aggiungi %classname</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dimmi/mail/registration</name>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/standard/templates/mail/mail_pagelayout.tpl" line="119"/>
+        <source>%privacy_link_start%Informazioni sulla privacy%privacy_link_end%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/standard/templates/mail/mail_pagelayout.tpl" line="120"/>
+        <source>%terms_link_start%Termini di utilizzo%terms_link_end%</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dimmi/menu</name>
+    <message>
+        <location filename="control_dimmi.php" line="422"/>
+        <source>Informazioni</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="control_dimmi.php" line="428"/>
+        <source>Faq</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="control_dimmi.php" line="433"/>
+        <source>Privacy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="control_dimmi.php" line="438"/>
+        <source>Termini di utilizzo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="control_dimmi.php" line="445"/>
+        <source>Discussioni</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="control_dimmi.php" line="454"/>
+        <source>Le mie attivitÃ </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="control_dimmi.php" line="467"/>
+        <source>Profilo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="control_dimmi.php" line="473"/>
+        <source>Notifiche</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="control_dimmi.php" line="483"/>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/dimmi/config.tpl" line="12"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="control_dimmi.php" line="490"/>
+        <source>Esci</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dimmi/navigator</name>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/dimmi/forum/replies_navigator.tpl" line="27"/>
+        <source>Visualizza commenti precedenti</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezstarrating/datatype</name>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/dimmi/forum/rating.tpl" line="7"/>
+        <source>Currently %current_rating out of 5 Stars.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/dimmi/forum/rating.tpl" line="13"/>
+        <source>Thank you for rating!</source>
+        <comment>When rating</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>openpa/workflow/event</name>
+    <message>
+        <location filename="openpadimmitype.php" line="12"/>
+        <source>Workflow Dimmi</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>openpa_dimmi/config</name>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/dimmi/config.tpl" line="36"/>
+        <source>Discussioni</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/dimmi/config.tpl" line="51"/>
+        <source>Cerca</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/dimmi/config.tpl" line="60"/>
+        <source>Esporta in CSV</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/dimmi/config.tpl" line="61"/>
+        <source>Aggiungi discussione</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>openpa_dimmi/dashboard</name>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/dimmi/dashboard.tpl" line="22"/>
+        <source>Le mie discussioni</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>openpa_dimmi/mail/post</name>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/standard/templates/openpa/mail_ezsubtreenotification/dimmi_forum_reply.tpl" line="30"/>
+        <source>Per disabilitare le notifiche email clicca %notification_link_start%qui%notification_link_end%</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>openpa_sensor/mail/post</name>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/standard/templates/openpa/mail_ezsubtreenotification/dimmi_forum_reply.tpl" line="48"/>
+        <source>Per disabilitare le notifiche email clicca %notification_link_start%qui%notification_link_end%</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>openpa_sensor/settings</name>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/notification/handler/ezsubtree/settings/edit.tpl" line="7"/>
+        <source>Notifiche aggiornamento discussioni</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/notification/settings.tpl" line="6"/>
+        <source>Impostazioni delle notifiche</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../git/openpa_dimmi/design/dimmi/templates/notification/settings.tpl" line="22"/>
+        <source>Salva le impostazioni</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
